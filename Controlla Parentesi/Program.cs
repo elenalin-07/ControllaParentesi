b4065786e5e018d52208ab84c8a4e3a0381ef897
elena <lin.elena@franchettisalviani.net>
2025-02-18 12:05:04 +0100

    prima ver

---
+++ Controlla Parentesi/Program.cs
@@ -0,0 +1,27 @@
+﻿bool controllaParentesi (string e)
+{
+    int a = 0, c = 0;
+    for (int i = 0; i < e.Length; i++)
+    {
+        if(e[i] == '(')
+        {
+            a++;
+        }
+        else if (e[i] == ')')
+        {
+            c++;
+            if (c > a)
+            {
+                return false;
+            }
+        }
+    }
+    if (e[0] == e[e.Length - 1] && e[0] == ')' && a != c)
+    {
+        return false;
+    }
+    return true;
+}
+Console.WriteLine("inserisci un'espressione numerica");
+string e = Console.ReadLine();
+Console.WriteLine(controllaParentesi(e));
